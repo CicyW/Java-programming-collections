4f915defd2904fdbd917f55b761706130a62e9c5
CicyW <wangchi327@163.com>
2018-03-30 19:51:29 +0800

    finished PracticeD of secction3

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,28 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Set<String> unique = new HashSet(collectionA);
+        Map<String, Integer> collection3 = new HashMap<String, Integer>();
+        for (String temp : unique) {
+            int num = 0;
+            if(temp.contains("-")){
+                num = Integer.parseInt(temp.substring(temp.indexOf('-')+1,temp.length()));
+                temp = temp.substring(0,temp.indexOf('-'));
+            }else{
+                num = Collections.frequency(collectionA, temp);
+            }
+            if(collection3.get(temp)!=null)
+                collection3.put(temp,collection3.get(temp)+num);
+            else
+                collection3.put(temp,num);
+        }
+        List<String> value = object.get("value");
+        for (String s:value){
+            collection3.put(s,collection3.get(s)-collection3.get(s)/3);
+        }
+        return collection3;
     }
 }
