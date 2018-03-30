e89d5bcd5d048467bf02b2e6697e5e82f06e412c
CicyW <wangchi327@163.com>
2018-03-30 19:50:14 +0800

    finished PracticeB of secction2

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,24 @@
 package cn.school.thoughtworks.section2;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Set<String> unique = new HashSet(collection1);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String temp : unique) {
+            int num = 0;
+            if(temp.contains("-")){
+                num = Integer.parseInt(temp.substring(temp.indexOf('-')+1,temp.length()));
+                temp = temp.substring(0,temp.indexOf('-'));
+            }else{
+                num = Collections.frequency(collection1, temp);
+            }
+            if(map.get(temp)!=null)
+                map.put(temp,map.get(temp)+num);
+            else
+                map.put(temp,num);
+        }
+        return map;
     }
 }
