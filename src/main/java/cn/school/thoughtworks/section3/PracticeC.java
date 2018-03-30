78f2c9a59071d7d38ae607926a33edd59fee994e
CicyW <wangchi327@163.com>
2018-03-30 19:51:14 +0800

    finished PracticeC of secction3

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,18 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Set<String> unique = new HashSet(collectionA);
+        Map<String, Integer> collection3 = new HashMap<String, Integer>();
+        for (String temp : unique) {
+            collection3.put(temp,Collections.frequency(collectionA, temp));
+        }
+        List<String> value = object.get("value");
+        for (String s:value){
+            collection3.put(s,collection3.get(s)-collection3.get(s)/3);
+        }
+        return collection3;
     }
 }
