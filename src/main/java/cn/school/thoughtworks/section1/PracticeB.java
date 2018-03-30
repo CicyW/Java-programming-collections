41258c15ac078d10eac71293e5ea37dcb31efae2
CicyW <wangchi327@163.com>
2018-03-30 19:49:16 +0800

    finished PracticeB of secction1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> intersections = new ArrayList<String>();
+        for(List<String> l:collection2){
+            List<String> intersection = collection1.stream().filter(item -> l.contains(item)).collect(toList());
+            intersections.addAll(intersection);
+        }
+        return intersections;
     }
 }
